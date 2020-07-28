0d74356ec7433f79447b8ba02834645fa6ff6aad
omarBnZaky <omarbnzaku@gmail.com>
2020-07-28 13:15:38 +0300

    Insertion & Deletion Singly Linked List

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -13,6 +13,7 @@ class LinkedList:
             print(cur_node.data)
             cur_node = cur_node.next
 
+    """ insertion methods"""
     def prepend(self,data):
         #add the node to the start of the list
         new_node = Node(data)
@@ -37,8 +38,40 @@ class LinkedList:
         new_node = Node(data)
         new_node.next = prev_node.next
         prev_node.next = new_node
+    
+    """Deletion Methods """
+    def delete_node(self,key):
+        cur_node = self.head
+        if cur_node and  cur_node.data == key:
+            self.head = cur_node.next
+            cur_node = None
+            return
 
+        prev = None
+        while cur_node and cur_node.data != key:
+            prev = cur_node
+            cur_node = prev.next
 
+        if cur_node is not None:
+            prev.next = cur_node.next
+        
+    def delete_node_at_postion(self,pos):
+        cur_node = self.head
+        if pos == 0:
+            cur_node = cur_node.next
+            return
+        prev = None
+        count = 1
+        while cur_node and count !=pos:
+            prev = cur_node
+            cur_node = prev.next
+            count+=1
+        
+        if cur_node is None:
+            return
+
+        prev.next = cur_node.next
+        cur_node = None
 
 llist = LinkedList()
 llist.append("A")
@@ -47,4 +80,6 @@ llist.append("D")
 llist.append("C")
 llist.prepend("J")
 llist.insert_after_node(llist.head.next,"R")
+llist.delete_node("A")
+llist.delete_node_at_postion(0)
 llist.print_list()
